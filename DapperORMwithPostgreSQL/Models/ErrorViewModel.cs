478006758820991f09e497c4a68f67127db907b4
Shibu Tamang <lamashibu19@gmail.com>
2018-09-19 12:00:21 +0545

    Add project files.

---
+++ DapperORMwithPostgreSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DapperORMwithPostgreSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
